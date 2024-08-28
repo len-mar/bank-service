66e9471f3c4f147f8acacbd52922ed8d85886d46
marleneanton <the.marlene.anton@gmail.com>
2024-08-28 16:25:14 +0200

    initial commit

---
+++ src/main/java/org/example/Account.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.math.BigDecimal;
+
+public class Account {
+    private String accountNumber;
+    private BigDecimal accountBalance;
+    private Client client;
+}
